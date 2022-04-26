7c683c814aaaa36c9772d7cdb297674e238b1510
Romain Equixor <r.equixor@gmail.com>
2022-04-27 00:46:45 +0400

    debut home page

---
+++ php/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> HOME PAGE </title>
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+<body>
+
+    <img src="" alt="badge">
+
+    <div class="featuredRecipe">
+        <div class="featureRecipeGauche">
+            <div class="hotRecipes">
+                <img src="../img/image_14.png" alt="mini Liste">
+                <p>Hot Recipes</p>
+            </div>
+
+            <h1>Spicy delicious chicken wings</h1>
+
+            <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim</p>
+
+            <div class="timer">
+                <img src="../img/Vector.png" alt=" chronomètre">
+                <p>30 Minutes</p>
+            </div>
+
+            <div class="chicken">
+                <div class="couvert">
+                    <img src="../img/fourchette.png" alt=" couvert">
+                    <img src="../img/couteau.png" alt=" couvert">
+                </div>
+                
+                <p>Chiken</p>
+            </div>      
+            
+            <div class="jhonSmith">
+                <img src="../img/avatar.png" alt=" image avatar">
+                <h1>John Smith</h1>
+                <p>15 March 2022</p>
+            </div>
+
+            <div class="viewRecipes">
+                <p>View Recipes</p>
+                <img src="../img/play.png" alt="flèche">
+            </div>
+        </div>
+
+        <div class="maskGroupe">
+
+            <!-- image en background -->
+            <img src="" alt="plat de poulet">
+        </div>
+        
+    </div>
+   
+</body>
+</html>
