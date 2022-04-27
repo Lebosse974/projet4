befa4bf384f1bb6d8a42f585547b2a93949d7165
Romain Equixor <r.equixor@gmail.com>
2022-04-28 02:11:16 +0400

    premiere recette afficher niveaux html css

--- php/index.php
+++ php/index.php
@@ -9,6 +9,13 @@
 </head>
 <body>
 
+<!-- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+<!-- --------------------------------------------------------------------------------- |      MON MAIN     |---------------------------------------------------------------------------- -->
+<!-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ -->
+
+
+
+<!-- ------------------------------------------------| premiére reccette afficher |--------------------------------------------- -->
     <img src="" alt="badge">
 
     <div class="featuredRecipe">
@@ -18,43 +25,51 @@
                 <p>Hot Recipes</p>
             </div>
 
-            <h1>Spicy delicious <br> chicken wings</h1>
+            <h1 class="textelorem">Spicy delicious <br> chicken wings</h1>
 
-            <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim</p>
+            <p class="textelorem">Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim</p>
 
-            <div class="timer">
-                <img src="../img/Vector.png" alt=" chronomètre">
-                <p>30 Minutes</p>
-            </div>
-
-            <div class="chicken">
-                <div class="couvert">
-                    <img src="../img/fourchette.png" alt=" couvert">
-                    <img src="../img/couteau.png" alt=" couvert">
+            <div class="timecouvert">
+                <div class="timer">
+                    <img src="../img/Vector.png" alt=" chronomètre">
+                    <p>30 Minutes</p>
                 </div>
-                
-                <p>Chiken</p>
-            </div>      
-            
-            <div class="jhonSmith">
-                <img src="../img/avatar.png" alt=" image avatar">
-                <h1>John Smith</h1>
-                <p>15 March 2022</p>
-            </div>
 
-            <div class="viewRecipes">
-                <p>View Recipes</p>
-                <img src="../img/play.png" alt="flèche">
+                <div class="chicken">
+                    
+                    <img  class="fourchette" src="../img/fourchette.png" alt=" couvert">
+                    <img class="couteau" src="../img/couteau.png" alt=" couvert">
+                    
+                    
+                    <p>Chiken</p>
+                </div>  
+            </div>
+                   
+            <div class="jhonView">
+                <div class="jhonSmith">
+                    <img src="../img/avatar.png" alt=" image avatar">
+                    <div class="titreTexteJhonSmith">
+                        <h1>John Smith</h1>
+                        <p>15 March 2022</p>
+                    </div>  
+                </div>
+
+                <div class="viewRecipes">
+                    <p>View Recipes</p>
+                    <img src="../img/play.png" alt="flèche">
+                </div>
             </div>
         </div>
+            
 
         <div class="maskGroupe">
 
             <!-- image en background -->
-            <img src="" alt="plat de poulet">
+            <img src="../img/platchiken.png" alt="plat de poulet">
         </div>
         
     </div>
+<!-- ------------------------------------------------------------------------------------------------------------------------------------------------ -->
    
 </body>
 </html>
